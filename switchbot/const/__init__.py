b131d34d6626ad33ca4a2736ffc54ec6c0d58580
XiaoLing-git <samuel.ling.xiao@qq.com>
2026-02-09 15:56:24 +0800

    add const

--- switchbot/const/__init__.py
+++ switchbot/const/__init__.py
@@ -97,6 +97,8 @@ class SwitchbotModel(StrEnum):
     PLUG_MINI_EU = "Plug Mini (EU)"
     RGBICWW_STRIP_LIGHT = "RGBICWW Strip Light"
     RGBICWW_FLOOR_LAMP = "RGBICWW Floor Lamp"
+    RGBIC_Neon_Rope_Light = "RGBIC Neon Rope Light"
+    RGBIC_Neon_Wire_Rope_Light = "RGBIC Neon Wire Rope Light"
     K11_VACUUM = "K11+ Vacuum"
     CLIMATE_PANEL = "Climate Panel"
     SMART_THERMOSTAT_RADIATOR = "Smart Thermostat Radiator"
